e9110e4dc44b56186bd289af1dd928b68dd0d7c2
DarthRevan02 <aadiankitjain05@gmail.com>
2025-10-24 01:31:51 +0530

    Add demonstration for records with distance calculation

--- Java/Java21Features.java
+++ Java/Java21Features.java
@@ -86,4 +86,15 @@ public class Java21Features {
         
         System.out.println("All virtual threads have been started");
     }
+
+    // Small, compatible demonstration for records that works on Java 21 and earlier
+    static void demonstrateRecords() {
+        System.out.println("\n5. Records:");
+        Point p1 = new Point(3, 4);
+        Point p2 = new Point(0, 0);
+        System.out.println("Point 1: " + p1);
+        System.out.println("Point 1 distance from origin: " + Math.sqrt(p1.x() * p1.x() + p1.y() * p1.y()));
+        System.out.println("Point 2: " + p2);
+        System.out.println("Point 2 distance from origin: " + Math.sqrt(p2.x() * p2.x() + p2.y() * p2.y()));
+    }
 }
